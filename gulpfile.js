619edd3abbb14ca05775af7242dad1e6c981aabf
jinghui chen <v10258@qq.com>
2015-06-26 13:46:19 +0800

    gulp auto tool release function

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,88 @@
-
-
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
-
-var path = require('path');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
+var spritesmith = require('gulp.spritesmith');
+
 var paths = {
+    dev: 'assets',
     css: 'assets/css/**/*.css',             // css 开发文件
-    js: 'assets/js/**/*.js',            // js 开发文件
-    less: 'less/**/*.less',             // less 开发文件
-    html: '*.html',                     // html 开发文件
-    lessLib: 'less/lib',                // less @import目录
-    build: 'build',                     // release 构建目录
-    devCss: 'assets/css'                // less 生成指向css开发目录
+    js: 'assets/js/**/*.js',                // js 开发文件
+    img: 'assets/img/**/*.+(png|jpg|gif)',  // img 开发文件
+    sprite: 'assets/img/**/**.png',         // 背景图片用于spritesheet转换
+    less: 'less/**/*.less',                 // less 开发文件
+    html: '*.html',                         // html 开发文件
+    lessLib: 'less/lib',                    // less @import目录
+    build: 'build',                         // release 构建目录
+    devCss: 'assets/css'                    // less 生成指向css开发目录
 };
 
 
 // 检查脚本
-gulp.task('lint', function(){
-	return gulp.src(paths.js)
-		.pipe(plugins.jshint())
-		.pipe(plugins.jshint.reporter('default'));
+gulp.task('lint', function() {
+    console.log(plugins.util.colors.green('Linting'));
+
+    return gulp.src(paths.js)
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('default'));
 });
 
 
 
 // 压缩脚本
-gulp.task('minify-js', function(){
-    gulp.src(paths.js)
+gulp.task('minify-js', function() {
+    console.log(plugins.util.colors.green('Minify js'));
+
+    return gulp.src(paths.js)
         .pipe(plugins.uglify())
         .pipe(gulp.dest(paths.build + '/js'));
 });
 
+
+// 编译Less
+gulp.task('less', function() {
+    console.log(plugins.util.colors.green('Compile less into CSS'));
+
+    return gulp.src(paths.less)
+        .pipe(plugins.less({
+            paths: paths.lessLib
+        }))
+        .pipe(gulp.dest(paths.devCss))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+// 解析CSS文件并且添加浏览器前缀到CSS规则里
+gulp.task('default', function() {
+    console.log(plugins.util.colors.green('Autoprefixer'));
+
+    return gulp.src(paths.css)
+        .pipe(plugins.autoprefixer({
+            browsers: ['> 5%'],
+            cascade: false
+        }))
+        .pipe(paths.dev + '/css');
+});
+
+// 生成css sprite 图片和样式表
+gulp.task('sprite', function() {
+    console.log(plugins.util.colors.green('Sprite generation'));
+
+    var spriteData = gulp.src(paths.sprite)
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'sprite.css'
+        }));
+    return spriteData.pipe(gulp.dest(paths.build));
+});
+
+
 // 压缩css
 gulp.task('minify-css', function(e) {
-    console.log(e, 'Minify css to ' + paths.build);
+    console.log(plugins.util.colors.green('Minify css to ' + paths.build));
 
     return gulp.src(paths.css)
         .pipe(plugins.minifyCss({
@@ -47,26 +92,46 @@ gulp.task('minify-css', function(e) {
 });
 
 // 压缩图片
+gulp.task('images', function() {
+    console.log(plugins.util.colors.green('Minify images'));
 
-// 编译Less
-gulp.task('less', function() {
-    console.log('Compile less into CSS');
-
-    return gulp.src(paths.less)
-        .pipe(plugins.less({
-            paths: [ path.join(__dirname, paths.lessLib)]
+    gulp.src(paths.img)
+        .pipe(plugins.imagemin({
+            progressive: true
         }))
-        .pipe(gulp.dest(paths.devCss))
-        .pipe(reload({stream: true}));
+        .pipe(gulp.dest(paths.build + '/img'));
 });
 
+
 // 发布
-gulp.task('release', function(){
+gulp.task('release', function() {
+    console.log(plugins.util.colors.green('Project release'));
+
+    // js检查压缩
     gulp.src(paths.js)
         .pipe(plugins.jshint())
         .pipe(plugins.jshint.reporter('default'))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(paths.build));
+        .pipe(gulp.dest(paths.build + '/js'));
+
+    // css前缀处理 压缩
+    gulp.src(paths.css)
+        .pipe(plugins.autoprefixer({
+            browsers: ['> 5%'],
+            cascade: false
+        }))
+        .pipe(plugins.minifyCss({
+            compatibility: 'ie7'
+        }))
+        .pipe(gulp.dest(paths.build + '/css'));
+
+    // css 图片压缩优化
+    gulp.src(paths.img)
+        .pipe(plugins.imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest(paths.build + '/img'));
+
 });
 
 
