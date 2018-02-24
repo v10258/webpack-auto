71be20dc972fbf51e4f4bb1ba983252ddb854ca7
niefeng <v10258@qq.com>
2018-02-24 14:12:48 +0800

    配置调整

---
+++ webpack.build.js
@@ -0,0 +1,144 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+const argv = require('yargs').argv;
+let isProduction = process.env.NODE_ENV === 'production';
+
+// 构建目录，构建入口, 如果没有指定模块则默认全部模块
+var submodule = argv.define || 'all';
+var entryFileName = submodule;
+var modules = ['find'];
+var submoduleSet;
+
+
+var outputPath = path.resolve(__dirname, 'public/');
+
+var config = {
+  entry: {
+    vendor: ['vue', 'vuex', 'axios']
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: outputPath
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+      })
+    }, {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'postcss-loader', 'sass-loader']
+      })
+    }, {
+      test: /\.ejs$/,
+      use: 'ejs-compiled-loader'
+    },
+    {
+      test: /\.vue$/,
+      use: 'vue-loader'
+    },
+    {
+      test: /\.js$/,
+      use: 'babel-loader'
+    }, {
+      test: /\.(png|jpg|jpeg|gif)(\?.+)?$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[ext]',
+          publicPath: '../',
+          outputPath: 'img/'
+        }
+      }]
+    }, {
+      test: /\.(eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 1000,
+          name: '[name].[ext]',
+          publicPath: '../',
+          outputPath: 'font/'
+        }
+      }]
+    }]
+  },
+  resolve: {
+    alias: {
+      mock: path.resolve(__dirname, 'src/common/js/mock/mock.js'),
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  plugins: [
+    // new webpack.ProvidePlugin({
+    //   $: 'jquery',
+    //   jQuery: 'jquery',
+    //   //Popper: ['popper.js', 'default']
+    // }),
+    new CopyWebpackPlugin([
+      { from: 'node_modules/bootstrap/dist/css', to: 'vendor/bootstrap/css/' }
+    ]),
+    // 抽出样式
+    new ExtractTextPlugin({
+      filename: 'css/[name].css'
+    }),
+    // 抽出公共文件vendor依赖，manifest运行时信息
+    new webpack.optimize.CommonsChunkPlugin({
+      //name: ['vendor', 'manifest']
+      name: 'vendor',
+      minChunks: Infinity,
+    }),
+    // 定义组件内环境变量
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+};
+
+console.log('submodule', submodule)
+if (submodule === 'all') {
+
+  modules.forEach((n, i) => {
+    submoduleSet = n.split('-');
+    submodule = submoduleSet.length === 2 ? submoduleSet[0] : n;
+    entryFileName = n;
+
+    config.entry[entryFileName] = path.resolve(__dirname, 'src/', submodule, entryFileName + '.js');
+
+    // //页面集成
+    config.plugins.push(new HtmlWebpackPlugin({
+      chunks: ['vendor', entryFileName],
+      template: path.resolve(__dirname, 'src/', submodule, entryFileName + '.ejs'),
+      filename: 'template/' + entryFileName + '.html'
+    }));
+
+    console.log('entry', path.resolve(__dirname, 'src/', submodule, entryFileName + '.js'));
+  })
+} else {
+  submoduleSet = submodule.split('-');
+  entryFileName = submodule;
+  submodule = submoduleSet.length === 2 ? submoduleSet[0] : submodule;
+
+  config.entry[entryFileName] = path.resolve(__dirname, 'src/', submodule, entryFileName + '.js');
+
+  //页面集成
+  config.plugins.push(new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src/', submodule, entryFileName + '.ejs'),
+    filename: 'template/' + entryFileName + '.html'
+  }));
+  console.log('entry', path.resolve(__dirname, 'src/', submodule, entryFileName + '.js'));
+}
+
+module.exports = config;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,23 +1,50 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+const argv = require('yargs').argv;
+let isProduction = process.env.NODE_ENV === 'production';
+
+// 构建目录，构建入口, 如果没有子目录 index 改为空字符串即可
+var submodule = argv.define || 'index';
+var entryFileName = submodule;
+
+// 子目录下有多个文件，入口为 centre-question 时
+var submoduleSet = submodule.split('-');
+if (submoduleSet.length === 2) {
+  submodule = submoduleSet[0];
+}
+
+var outputPath = path.resolve(__dirname, 'public/', submodule);
+var entryPath = path.resolve(__dirname, 'src/', submodule, entryFileName + '.js');
 
 var config = {
   entry: {
-    vendor: ['lodash']
+    vendor: ['vue', 'vuex', 'axios', 'mock']
   },
   output: {
     filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: outputPath
   },
   module: {
     rules: [{
       test: /\.css$/,
       use: ['style-loader', 'css-loader']
     }, {
-      test: /\.ejs$/,
-      use: 'ejs-compiled-loader'
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
     }, {
+      test: /\.ejs$/,
+      use: ['ejs-compiled-loader']
+    },
+    {
+      test: /\.vue$/,
+      use: 'vue-loader'
+    },
+    {
       test: /\.js$/,
       use: 'babel-loader'
     }, {
@@ -26,26 +53,49 @@ var config = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: 'img/[name].[ext]'
+          name: '[name].[ext]'//,
+          //useRelativePath: isProduction
         }
       }]
-    },{
+    }, {
       test: /\.(eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
       use: [{
         loader: 'url-loader',
         options: {
-          limit: 100000,
-          name: 'font/[name].[ext]'
+          limit: 1000,
+          name: '[name].[ext]'//,
+          //useRelativePath: isProduction
         }
       }]
     }]
   },
+  resolve: {
+    alias: {
+      mock: path.resolve(__dirname, 'src/common/js/mock/mock.js'),
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  // externals: {
+  //   '$': 'window.$',
+  //   'jQuery': 'window.jQuery'
+  // },
   plugins: [
+    // new webpack.ProvidePlugin({
+    //   $: 'jquery',
+    //   jQuery: 'jquery',
+    //   //Popper: ['popper.js', 'default']
+    // }),
+    new CopyWebpackPlugin([
+      { from: 'node_modules/bootstrap/dist/css', to: 'vendor/bootstrap/css/' }
+    ]),
     // 页面集成
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve('src/', submodule, entryFileName + '.ejs')
+    }),
     // 抽出公共文件vendor依赖，manifest运行时信息
     new webpack.optimize.CommonsChunkPlugin({
-      name: ['vendor', 'manifest']
+      //name: ['vendor', 'manifest']
+      name: 'vendor'
     }),
     // 模块热更新
     new webpack.HotModuleReplacementPlugin()
@@ -66,15 +116,9 @@ var config = {
   }
 };
 
-// 构建目录，构建入口
-var submodule = process.argv[5] || 'index';
-var entryFileName = submodule;
+config.entry[entryFileName] = entryPath;
 
-// 输入输出设置
-config.entry[entryFileName] = path.resolve(__dirname, 'src/', submodule, entryFileName + '.js');
-config.output.path = path.resolve(__dirname, 'dist/', submodule);
-config.plugins[0] = new HtmlWebpackPlugin({
-  template: path.resolve('src/', submodule, entryFileName + '.ejs')
-})
+console.log('entry', path.resolve(__dirname, 'src/', submodule, entryFileName + '.js'));
+console.log('template', path.resolve('src/', submodule, entryFileName + '.ejs'));
 
 module.exports = config;
